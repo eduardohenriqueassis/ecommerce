a509912d6f504467218cf9fad39822a11ffaf593
Eduardo Assis <eduardohenriqueassis1973@gmail.com>
2021-08-16 01:03:59 -0300

    criando a page admin

--- vendor/hcodebr/php-classes/src/page.php
+++ vendor/hcodebr/php-classes/src/page.php
@@ -12,13 +12,13 @@ class Page {
         "data"=>[]
     ];
 
-    public function __construct($opts = array()){
+    public function __construct($opts = array(), $tpl_dir = "/views/"){
 
         $this->options = array_merge($this->defaults, $opts);
 
             // config
         $config = array(
-            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",  //Variável de sistema para dizer para o tpl procurar o root.
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,  //Variável de sistema para dizer para o tpl procurar o root.
             "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
             "debug"         => false // set to false to improve the speed
         );
