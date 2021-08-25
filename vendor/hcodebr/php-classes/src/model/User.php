4f2bd58e07bc1a49731c2c77ddd886f01800760b
Eduardo Assis <eduardohenriqueassis1973@gmail.com>
2021-08-25 01:25:18 -0300

    Create Users

--- vendor/hcodebr/php-classes/src/model/User.php
+++ vendor/hcodebr/php-classes/src/model/User.php
@@ -62,10 +62,53 @@ class User extends Model {
 
     }
 
+    public function get($iduser)
+    {
+     
+        $sql = new Sql();
+     
+        $results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser;", array(
+            ":iduser"=>$iduser
+
+        ));
+     
+        $data = $results[0];
+     
+        $this->setData($data);
+     
+    }
+
     public static function logout()
     {
 
         $_SESSION[User::SESSION] = NULL;
+
+    }
+
+    public static function listAll(){
+
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+
+    }
+
+    public function save()
+    {
+
+        $sql = new Sql();
+
+        $results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)",
+            array(
+            ":desperson"=>$this->getdesperson(),
+            ":deslogin"=>$this->getdeslogin(),
+            ":despassword"=>$this->getdespassword(),
+            ":desemail"=>$this->getdesemail(),
+            ":nrphone"=>$this->getnrphone(),
+            ":inadmin"=>$this->getinadmin()
+        ));
+
+        $this->setData($results[0]); 
 
     }
 
